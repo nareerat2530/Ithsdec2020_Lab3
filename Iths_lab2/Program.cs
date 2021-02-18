639eb98821bf62affa39266474397881fcb063f7
nareerat2530 <66684500+nareerat2530@users.noreply.github.com>
2021-02-18 23:22:04 +0100

    Update Program.cs

--- Iths_lab2/Program.cs
+++ Iths_lab2/Program.cs
@@ -9,6 +9,7 @@ namespace Iths_lab2
     internal class Program
     {
         private static readonly List<Shape> ShapeList = new();
+        
         static void Main(string[] args)
         {
             Console.WriteLine();
@@ -25,6 +26,7 @@ namespace Iths_lab2
             PrintOutTrianglePoints();
             Console.WriteLine("\n_____________________________________________________________________");
         }
+        
         private static void GenerateAndPrintOutAllTheShapes()
         {
             for (var i = 0; i < 20; i++)
@@ -36,6 +38,7 @@ namespace Iths_lab2
                 Console.WriteLine(shape);
             }
         }
+        
         private static float CalculateTriangleCircumference(IEnumerable<Shape> listOfShapes)
         {
             var totalTriangleCircumference = 0.00f;
@@ -48,12 +51,14 @@ namespace Iths_lab2
             }
             return totalTriangleCircumference;
         }
+        
         private static float CalculateAverageAreaOfAllShape(IEnumerable<Shape> listOfShape)
         {
             var averageArea = listOfShape.Sum(shapes => shapes.Area);
             var totalAverageArea = averageArea / ShapeList.Count;
             return totalAverageArea;
         }
+        
         private static float FindBiggestVolumeOfAllTheShape(IEnumerable<Shape> listOfShapes)
         {
             var maxVolume = 0.00f;
@@ -64,6 +69,7 @@ namespace Iths_lab2
             }
             return maxVolume;
         }
+        
         private static void PrintOutTrianglePoints()
         {
             var triangle = new Triangle(new Vector2(3.25f,6.77f), new Vector2(4.56f,6.41f), new Vector2(2.05f, 3.58f));
